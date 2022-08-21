f7670ae065dcc8635887c03f6affdf0366c4afd2
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2022-08-21 12:45:24 -0500

    03. Storing values with the assignment operator

--- Basic-JavaScript/script.js
+++ Basic-JavaScript/script.js
@@ -1,4 +1,4 @@
-// Comment your JavaScript Code
+// COMMENT YOUR JAVASCRIPT CODE
 
 // There are 2 ways to write comments in JavaScript
 // This is an inline comment
@@ -13,7 +13,7 @@ As you write code, you should regularly add comment to clarify
 the function of parts of your code.
 */
 
-// Declare JavaScript variables
+// DECLARE JAVASCRIPT VARIABLES
 
 /*
 JavaScript provides eight different data types, wich are:
@@ -47,4 +47,19 @@ var ourName;
 In JavaScript we end statements with semicolons.
 Variable names can be made up of numbers, letters, and $ or _,
 but may not contain spaces or start with a number.
+*/
+
+// STORGING VALUES WITH THE ASSIGNMENT OPERATOR
+
+myVariable = 3;
+
+// In JavaScript you can store a value in a variable with the assignment operator (=).
+
+var myVar;
+myVar = 7;
+
+/*
+First, this code creates a variable named myVar.
+Then, the codes assigns 7 to myVa. Now, if myVar appears
+again in the code, the program will treat it as if it is 7.
 */
