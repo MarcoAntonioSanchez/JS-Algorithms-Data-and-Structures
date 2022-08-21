5d1a33e4efbfff2d133f9de41531fc5a857e7e75
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2022-08-21 12:39:55 -0500

    02. Declare JavaScript variables

--- Basic-JavaScript/script.js
+++ Basic-JavaScript/script.js
@@ -1,7 +1,50 @@
+// Comment your JavaScript Code
+
 // There are 2 ways to write comments in JavaScript
 // This is an inline comment
 
 /*
 This a multi-line comment.
 For example.
+*/
+
+/*
+As you write code, you should regularly add comment to clarify
+the function of parts of your code.
+*/
+
+// Declare JavaScript variables
+
+/*
+JavaScript provides eight different data types, wich are:
+undefinde, null, boolean, string, symbol, biginit, number and ob ject
+
+Computers can perform mathematical operations on a number,
+but not on a string.
+
+For example:
+"22" and 22 are not the same, the 1st one is a string type
+and the 2nd 22 is a number type.
+
+Variables allow computers to store and manipulate data
+in a dynamicfashion.
+Any of the eight data types may be stored in a variable.
+
+Variables are similar to the x and y variables you use in
+mathematics, wich means they're simple name to represent
+the data we eant to refer to.
+COmputer variables differ from mathematical variables in that
+they can store different values at different times.
+*/
+
+// We tell JavaScript to create or declare a variable:
+
+var ourName;
+
+// creates a variable called ourName.
+
+/*
+In JavaScript we end statements with semicolons.
+Variable names can be made up of numbers, letters, and $ or _,
+but may not contain spaces or start with a number.
 */
