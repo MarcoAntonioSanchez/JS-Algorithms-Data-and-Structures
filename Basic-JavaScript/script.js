ee03aa3ad715cf15701cc96e0768c4a5828d1b97
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2022-08-21 16:25:42 -0500

    41. Modify array data with indexes

--- Basic-JavaScript/script.js
+++ Basic-JavaScript/script.js
@@ -613,3 +613,15 @@ const array = [50, 60, 70];
 console.log(array[0]);
 const data = array[1];
 console.log(data);
+
+// MODIFY ARRAY DATA WITH INDEXES
+
+/*
+Unlike strings, the entries of arrays are mutable and can be
+changed freely, even if the array was declared with const.
+For example:
+*/
+
+const ourArray = [50, 40, 30];
+ourArray[0] = 15;
+console.log(ourArray);
