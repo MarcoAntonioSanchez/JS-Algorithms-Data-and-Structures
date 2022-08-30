88ebda78f199c1db740477a19902349f5d9ebc0c
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2022-08-29 22:49:39 -0500

    58. Use conditional logic with if statements

--- Basic-JavaScript/script.js
+++ Basic-JavaScript/script.js
@@ -890,3 +890,38 @@ These two states are mutually exclusive.
 
 Boolean values are never written with quotes.
 */
+
+// USE CONDITIONAL LOGIC WITH IF STATEMENTS
+
+/*
+if Statements are used to make decisions in code.
+The keyword if tells JavaScript to execute the code in the curly braces under certain conditions,
+defined in the parentheses.
+These conditions are known as boolean conditions and the may only be true or false.
+
+When the condition evaluates to true, the program executes the statement inside the curly braces.
+When the boolean condition evaluates to false, the statement inside the curly braces will not execute.
+
+if (condition is true) {
+  statement is executed
+}
+
+For example:
+*/
+
+function test(myCondition) {
+  if (myCondition) {
+    return "It was true";
+  }
+  return "It was false";
+}
+
+console.log(test(true));
+console.log(test(false));
+
+/*
+test(true) Returns the string "It was true", and test(false) returns the string "It was false".
+When test is called with a value of true, the if statement evaluates myCondition to see if it is true
+or not. SInce it is true, the function returns "It was true". When we call test with a value of false,
+myCondition is not true and the statement in the curly braces is not executed and the function returns "It was false".
+*/
