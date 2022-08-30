5a4571fedfdf416d986acaa82dcef927bf22e4f4
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2022-08-29 23:31:35 -0500

    65. Comparison  with the less than operator

--- Basic-JavaScript/script.js
+++ Basic-JavaScript/script.js
@@ -1046,3 +1046,19 @@ Examples:
 2 >= 3 // false
 '7' >= 9 // false
 */
+
+// COMPARISON WITH THE LESS THAN OPERATOR
+
+/*
+The less than operator (<) compares the values of two numbers. If the number to the left
+is less than the number to the right, it returns true. Otherwise, it returns false.
+Like the equality operator, the less than operator converts data types while comparing.
+
+Examples:
+
+2 < 5 // true
+'3' < 7 // true
+5 < 5 // false
+3 < 2 // false
+'8' < 4 // false
+*/
