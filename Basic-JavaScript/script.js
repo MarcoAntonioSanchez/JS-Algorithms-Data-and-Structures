47da7dcbc8b65061fd41d7dd3fc70a3d3d92e1c4
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2022-08-30 22:35:35 -0500

    70. Space - Introducing else if statements

--- Basic-JavaScript/script.js
+++ Basic-JavaScript/script.js
@@ -1142,3 +1142,19 @@ if (num > 10) {
   return "10 or Less";
 }
 */
+
+// INTRODUCING ELSE IF STATEMENTS
+
+/*
+If you have multiple conditions that need to be addressed, you can chain if statements
+together with else if statements.
+
+if (num > 15) {
+  return "Bigger than 15";
+} else if (num < 5) {
+  return "Smaller than 5";
+} else {
+  return "Between 5 and 15";
+}
+
+*/
