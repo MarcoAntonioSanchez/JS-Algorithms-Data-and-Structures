ddb08a8c8c3a8d736d3a547549aaa1f43aa5ceb5
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2022-08-30 22:47:48 -0500

    71. Logical order in if else statements

--- Basic-JavaScript/script.js
+++ Basic-JavaScript/script.js
@@ -1156,5 +1156,42 @@ if (num > 15) {
 } else {
   return "Between 5 and 15";
 }
+*/
 
+// LOGICAL ORDER IN IF ELSE STATEMENTS
+
+/*
+Order is important in if, else if statements.
+The function is executed from top to bottom so you will want to be careful of what statement comes first.
+
+Take these two functions as an example:
+*/
+
+function foo(x) {
+  if (x < 1) {
+    return "Less than one";
+  } else if (x < 2) {
+    return "Less than two";
+  } else {
+    return "Greater than or equal to two";
+  }
+}
+
+console.log(foo(3));
+
+function bar(x) {
+  if (x < 2) {
+    return "Less than two";
+  } else if (x < 1) {
+    return "Less than one";
+  } else {
+    return "Greater than or equal to two";
+  }
+}
+
+console.log(bar(0));
+
+/*
+While these two functions look nearly identical if we pass a number to both
+we get different outputs.
 */
